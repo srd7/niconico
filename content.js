9517c9e9b66f11edb8747749d896019b3869589c
srd7 <kuroneko0011@gmail.com>
2016-02-08 01:50:35 +0900

    hide ad area...

--- content.js
+++ content.js
@@ -1,9 +1,17 @@
-(function close(){
-  var button = $(".excludeReserveButton")[0],
-      chekker = $(".enableHistory");
-  if(chekker[0] && !chekker.hasClass("excludeReserved") && button){
-    button.click();
-    console.info("closed");
+(function (){
+  // Hide the ad area.
+  $(".textMarqueeOuter").hide();
+  console.info("[Niconico Ad Closer] Hiding the ad area.")
+  // Click "X" button if there is each 100 ms.
+  function close() {
+    var button = $(".excludeReserveButton")[0],
+        chekker = $(".enableHistory");
+    if(chekker[0] && !chekker.hasClass("excludeReserved") && button){
+      button.click();
+      console.info("[Niconico Ad Closer] Closed an ad");
+    }
+    setTimeout(close, 500);
   }
-  setTimeout(close, 100);
+
+  close();
 })();
