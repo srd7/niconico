e3d4d5a1847b3b9b7be31997f54a1da25affbede
srd7 <kuroneko0011@gmail.com>
2016-12-22 21:45:02 +0900

    handle HTML5 player

--- content.js
+++ content.js
@@ -1,14 +1,29 @@
 (function (){
+  var isHTML5Player = $(".textMarqueeOuter").length === 0;
   // Hide the ad area.
-  $(".textMarqueeOuter").hide();
-  console.info("[Niconico Ad Closer] Hiding the ad area.")
-  // Click "X" button if there is each 100 ms.
+  if (isHTML5Player) {
+    $(".Marquee-item").hide();
+    console.info("[Niconico Ad Closer] Hiding the ad area for HTML5 Player.");
+  } else {
+    $(".textMarqueeOuter").hide();
+    console.info("[Niconico Ad Closer] Hiding the ad area.");
+  }
+  // Click "X" button if there is each 500 ms.
   function close() {
-    var button = $(".excludeReserveButton")[0],
-        chekker = $(".enableHistory");
-    if(chekker[0] && !chekker.hasClass("excludeReserved") && button){
-      button.click();
-      console.info("[Niconico Ad Closer] Closed an ad");
+    var button, checker;
+    if (isHTML5Player) {
+      button = $(".DefaultAnimator-excludeButton")[0];
+      if (button) {
+        button.click();
+        console.info("[Niconico Ad Closer] Closed an ad.");
+      }
+    } else {
+      button = $(".excludeReserveButton")[0];
+      checker = $(".enableHistory");
+      if(checker[0] && !checker.hasClass("excludeReserved") && button){
+        button.click();
+        console.info("[Niconico Ad Closer] Closed an ad");
+      }
     }
     setTimeout(close, 500);
   }
