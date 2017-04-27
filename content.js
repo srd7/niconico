2bb6e0a14df337bb8aa588cac55a41a669a9ae36
srd7 <kuroneko0011@gmail.com>
2017-04-28 00:38:16 +0900

    fix selector

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
   var isHTML5Player = $(".textMarqueeOuter").length === 0;
   // Hide the ad area.
   if (isHTML5Player) {
-    $(".Marquee-item").hide();
+    $(".Marquee-itemArea").hide();
     console.info("[Niconico Ad Closer] Hiding the ad area for HTML5 Player.");
   } else {
     $(".textMarqueeOuter").hide();
@@ -12,7 +12,7 @@
   function close() {
     var button, checker;
     if (isHTML5Player) {
-      if ($(".Marquee-item").css("display") !== "none") {
+      if ($(".Marquee-itemArea").css("display") !== "none") {
         $(".Marquee-item").hide();
       }
       button = $(".DefaultAnimator-excludeButton")[0];
